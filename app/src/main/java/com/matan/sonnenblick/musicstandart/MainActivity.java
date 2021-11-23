19059f68eba0dfaaa4ceea019249b7df8c890cda
matansonn <matansonn@gmail.com>
2021-11-23 13:29:46 +0200

    Base version

--- app/src/main/java/com/matan/sonnenblick/musicstandart/MainActivity.java
+++ app/src/main/java/com/matan/sonnenblick/musicstandart/MainActivity.java
@@ -1,8 +1,11 @@
 package com.matan.sonnenblick.musicstandart;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new CountDownTimer(5000, 1000) {
+
+            @Override
+            public void onTick(long l) {
+                Toast.makeText(getApplicationContext(), "Loading...("+ (l/1000)+ ")",Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onFinish() {
+                Intent go = new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(go);
+
+            }
+        }.start();
     }
 }
